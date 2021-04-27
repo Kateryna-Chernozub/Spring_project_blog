63852cc27030b9470c01d12121940c0994b298c1
Katerynachernozub <68657369+Katerynachernozub@users.noreply.github.com>
2021-04-27 20:44:47 +0300

    add first controller adn view

--- src/main/java/com/firstproject/blog/BlogApplication.java
+++ src/main/java/com/firstproject/blog/BlogApplication.java
@@ -3,11 +3,12 @@ package com.firstproject.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
 }
